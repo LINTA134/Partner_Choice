4db19c622e69fdbe33fe25fb892f870095114e2f
Rinta134 <cgkh1017@mail2.doshisha.ac.jp>
2025-11-19 17:18:33 +0900

    App_01 was constructed

--- App_02_MainTask/__init__.py
+++ App_02_MainTask/__init__.py
@@ -1,13 +1,3 @@
 doc = """
 Your app description
 """
-
-"""
-page_sequence = [
-    P15_Wait_Game1,
-    P16_MessageInstruction,
-    P17_Wait_Message,
-    P18_Reputation_Gossip,
-    P19_Decision,
-]
-"""
